59aed1c11ad042102896a3cbda30500832f11f23
SUMANTI <SUMANTI@DESKTOP-S8MO377>
2022-05-26 09:59:56 +0530

    React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 style={{ color: "violet" }}>* Nature *</h1>
+  
+    <p style={{ color: "pink" }}>Nature is our defender, providing a variety of food, water, and many other things that cannot be explained. 
+      Without woods, rocks, animals, fruits, and trees, the environment would be devoid of beauty.
+      If we continue to pollute the environment, we shall soon come to an end.
+      We must constantly take precautions to preserve our environment; pollution and contamination levels must be kept under control - 
+      this is the duty of people who utilize nature on a regular basis.</p>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/image.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import img1 from './images/23277.jpg';
+
+function Imagess(){                    
+
+        return(
+            <div>
+            <img src={img1} alt=""/>
+            </div>
+        )
+    }
+
+export default Imagess;
